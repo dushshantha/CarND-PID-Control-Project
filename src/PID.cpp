b8d146c1b73f3cdbd3815b095018344a00c31214
dushshantha <dushshantha.chandaradasa@gmail.com>
2017-08-16 12:27:48 -0400

    modified the Twiddle to fix few issues

--- src/PID.cpp
+++ src/PID.cpp
@@ -23,14 +23,14 @@ void PID::Init(double Kp, double Ki, double Kd) {
     PID::sum_cte = 0.0;
     PID::prev_cte = 1.0;
     
-    PID::tol = 0.000001;
+    PID::tol = 0.0000001;
     
     //Set the below flag to true to run Twiddle
-    PID::doTwiddle = false;
+    PID::doTwiddle = true;
     
     PID::p = {0.0, 0.0, 0.0};
     //PID::dp = {1, 1, 1};
-    PID::dp = {Kp * 0.01, Ki* 0.008, Kd* 0.005};
+    PID::dp = {Kp, Ki, Kd};
     PID::index = 0;
     PID::it = 0;
     
@@ -55,53 +55,39 @@ void PID::Update(double cte){
         }
         // Step 1: Increase the param in index and let the
         // error update on that
-        std::cout << "step1Done : " << step1Done << std::endl;
-        if(!step1Done){
-            std::cout << "Step 1 Done" << std::endl;
-            p[index] += dp[index];
-            step1Done = true;
-        }
-        else if(!step2Done){
-            if(cte < best_err){
-                // got best error for the param in index
-                // Moving to the next param
-                best_err = cte;
-                dp[index] *= 1.1;
-                index = (index + 1) % 3;
+        std::cout << "Step 1 Done" << std::endl;
+        p[index] += dp[index];
+        step1Done = true;
+        
+        if(cte < best_err){
+            // got best error for the param in index
+            // Moving to the next param
+            best_err = cte;
+            
+            index = (index + 1) % 3;
                 
 
-                // Resetting the indicators for the next param
-                step1Done = false;
-                step2Done = false;
-                step3Done = false;
-            }
-            else {
+            
+        }
+        if(!step1Done){
+            dp[index] *= 1.1;
+            step1Done = true;
+        }
+        else if (!step2Done){
                 // No best error
                 // Reverse
                 p[index] -= 2 * dp[index];
                 step2Done =  true;
                 
-            }
         }
-        else if(!step3Done){
-            if(cte < best_err){
-                best_err = cte;
-                dp[index] *= 1.1;
+        else {
+            p[index] += dp[index];
+            dp[index] *= 0.9;
+            index = (index + 1) % 3;
+            step1Done = step2Done = false;
                 
-                index = (index + 1) % 3;
-                // Resetting the indicators for the next param
-                step1Done = false;
-                step2Done = false;
-                step3Done = false;
-
-            }
-            else {
-                p[index] += dp[index];
-                dp[index] *= 0.9;
-                //step3Done = true;
-                //step2Done = false;
-            }
         }
+            
         
         // Set the new Params for Error Update
         Kp = p[0];
@@ -114,7 +100,10 @@ void PID::Update(double cte){
     
     
     // DEBUG
-    std::cout << "Kp: " << Kp << " Ki: " << Ki << " Kd: " << Kd <<std::endl;
+    
+    std::cout << "Kp: " << Kp << " Ki: " << Ki << " Kd: " << Kd << " Index :" << index <<std::endl;
+    std::cout << "step1Done : " << step1Done << " step2Done : " << step2Done << " step3Done : " << step3Done <<std::endl;
+    std::cout << "dp 0: " << dp[0] << " dp 1: " << dp[1] << " dp 2: " << dp[2] << " Index :" << index <<std::endl;
 
     UpdateError(cte);
    
